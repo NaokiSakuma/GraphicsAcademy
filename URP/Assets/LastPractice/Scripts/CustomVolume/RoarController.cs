5cb1e4b0d0c369769c7e5f7a69d845568991eae5
NaokiSakuma <soramamenatan@gmail.com>
2022-04-23 03:07:24 +0900

    wip

---
+++ URP/Assets/LastPractice/Scripts/CustomVolume/RoarController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CustomVolume
+{
+    [ExecuteAlways]
+    public class RoarController : MonoBehaviour
+    {
+        public VolumeProfile volumeProfile;
+        [Range(0f, 100f)]
+        public float focusPower = 10f;
+        [Range(0, 10)]
+        public int focusDetail = 5;
+        public int referenceResolutionX = 1334;
+        public Vector2 focusScreenPosition = Vector2.zero;
+        RoarVolumeComponent roarVolumeComponent;
+
+        void Update()
+        {
+            if (volumeProfile == null) return;
+            if (roarVolumeComponent == null) volumeProfile.TryGet<RoarVolumeComponent>(out roarVolumeComponent);
+            if (roarVolumeComponent == null) return;
+
+            roarVolumeComponent.focusPower.value = focusPower;
+            roarVolumeComponent.focusDetail.value = focusDetail;
+            roarVolumeComponent.focusScreenPosition.value = focusScreenPosition;
+            roarVolumeComponent.referenceResolutionX.value = referenceResolutionX;
+        }
+    }
+}
---
+++ URP/Assets/LastPractice/Scripts/CustomVolume/RoarVolumeComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace CustomVolume
+{
+    public class RoarVolumeComponent : VolumeComponent, IPostProcessComponent
+    {
+        public FloatParameter focusPower = new(0f);
+        public IntParameter focusDetail = new(5);
+        public Vector2Parameter focusScreenPosition = new(Vector2.zero);
+        public IntParameter referenceResolutionX = new(1334);
+
+        public bool IsActive() => focusPower.value > 0f;
+        public bool IsTileCompatible() => false;
+    }
+}
