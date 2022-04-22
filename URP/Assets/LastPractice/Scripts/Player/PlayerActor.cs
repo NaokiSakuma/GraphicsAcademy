1161485e283d3ebbf8d3827bea700a3e96d7a238
NaokiSakuma <soramamenatan@gmail.com>
2022-04-22 12:27:14 +0900

    player

--- URP/Assets/LastPractice/Scripts/Player/PlayerActor.cs
+++ URP/Assets/LastPractice/Scripts/Player/PlayerActor.cs
@@ -1,4 +1,5 @@
 using SnowScene.Inputs;
+using SnowScene.Snow;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
@@ -24,6 +25,21 @@ namespace SnowScene.Player
                 {
                     var moveValue = new Vector3(x.x, 0, x.y);
                     transform.Translate(moveValue * Time.deltaTime * moveSpeed);
+                })
+                .AddTo(this);
+
+            this.UpdateAsObservable()
+                .Subscribe(_ =>
+                {
+                    RaycastHit hit;
+                    if(Physics.Raycast(transform.localPosition, Vector3.down, out hit))
+                    {
+                        var texDraw = hit.collider.gameObject.GetComponent<Footprint>();
+                        if (texDraw == null)
+                            return;
+
+                        texDraw.IndentAt(hit);
+                    }
                 })
                 .AddTo(this);
 
